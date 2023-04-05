00add90bf56920504a7014e3cd4a7db08fece763
ronariv <96355410+Ronariv@users.noreply.github.com>
2023-04-05 12:04:53 +0700

    Add files via upload

---
+++ ChangePreventers_Kel15/ChangePreventers_Kel15/src/divergent_change/before/Rectangle.java
@@ -0,0 +1,44 @@
+package divergent_change.before;
+
+public class Rectangle {
+	private int width;
+	private int height;
+	
+	public Rectangle(int width, int height) {
+		if(width <= 0 || height <= 0) {
+			throw new IllegalArgumentException("width and height must be greater than zero");
+		}
+		
+		this.width = width;
+		this.height = height;
+	}
+	
+	public int getWidth() {
+		return width;
+	}
+	
+	public int getHeight() {
+		return height;
+	}
+	
+	public int area() {
+		return width * height;
+	}
+	
+	public int perimeter() {
+		return 2 * (width + height);
+	}
+	
+	public void print(String style) {
+		style = style.toLowerCase();
+		if(style.equals("full")) {  //Print full rectangle
+			FullRectanglePrinter full = new FullRectanglePrinter(null);
+			full.print();
+		} else if(style.equals("border")) {
+			BorderRectanglePrinter border = new BorderRectanglePrinter(null);
+			border.print();
+		} else {
+			System.out.println("style not recognized");
+		}
+	}
+}
---
+++ ChangePreventers_Kel15/ChangePreventers_Kel15/src/paralell_inheritance_hierarchies/before/PrintTest.java
@@ -0,0 +1,20 @@
+package paralell_inheritance_hierarchies.before;
+
+import org.junit.jupiter.api.Test;
+
+class PrintTest {
+
+	@Test
+	void test() {
+		Rectangle r = new Rectangle();
+		r.setHeight(5);
+		r.setWidth(4);
+		
+		// Rectangle rac = new Rectangle();
+		
+		System.out.println(r.area());
+		
+		
+	}
+
+}
