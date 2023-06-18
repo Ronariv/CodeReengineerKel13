043322a36db5090fa9534fce930fc53d6b0bb9a5
ronariv <96355410+Ronariv@users.noreply.github.com>
2023-06-18 20:38:11 +0700

    Add files via upload

---
+++ Unnecessary/src/Cube.java
@@ -0,0 +1,12 @@
+public class Cube extends Shape {
+    private double side;
+
+    public Cube(double side) {
+        this.side = side;
+    }
+
+    @Override
+    public double calculateArea() {
+        return 6 * side * side;
+    }
+}
---
+++ Unnecessary/src/Sphere.java
@@ -0,0 +1,12 @@
+public class Sphere extends Shape {
+    private double radius;
+
+    public Sphere(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public double calculateArea() {
+        return 4 * Math.PI * radius * radius;
+    }
+}
